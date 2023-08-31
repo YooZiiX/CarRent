8d474c686413b1373159f958261cf6ac250df4ca
YooZiiX <jeremy.lucas@mines-ales.org>
2023-09-01 00:59:13 +0200

    add hero background overlay

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import HeroImage from "@/public/hero.png";
 import CustomButton from "./ui/CustomButton";
 
 const Hero = () => {
@@ -19,6 +20,12 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src={HeroImage} alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
