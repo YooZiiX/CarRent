7566d65010f2c0c232e0ea71f5d9859693f23a07
YooZiiX <jeremy.lucas@mines-ales.org>
2023-09-03 17:05:50 +0200

    add search btn

--- components/ui/SearchBar.tsx
+++ components/ui/SearchBar.tsx
@@ -1,15 +1,28 @@
 "use client";
 
 import Image from "next/image";
+import SearchLogo from "@/public/magnifying-glass.svg";
 import { SearchManufacturer } from "..";
 
 import { useState } from "react";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={SearchLogo}
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
 
   const handleSubmit = () => {
-    console.log("Submit");
+    console.log("");
   };
   return (
     <form className="searchbar" onSubmit={handleSubmit}>
@@ -18,6 +31,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hiden" />
       </div>
     </form>
   );
