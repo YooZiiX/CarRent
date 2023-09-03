79d9d1b24cf614629b49ef938239a637606c7cf3
YooZiiX <jeremy.lucas@mines-ales.org>
2023-09-03 21:46:31 +0200

    add Show More btn (client side)

--- components/ui/ShowMore.tsx
+++ components/ui/ShowMore.tsx
@@ -1,8 +1,24 @@
 "use client";
+import { ShowMoreProps } from "@/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { CustomButton } from "..";
 
-const ShowMore = () => {
-  return <div>ShowMore</div>;
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+  const handleNavigation = () => {};
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show more"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ShowMore;
