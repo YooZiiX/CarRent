eb403d54422ffa2211ff867cc2d4bb913cd27eb3
YooZiiX <jeremy.lucas@mines-ales.org>
2023-09-04 20:08:27 +0200

    add key => .env

--- utils/index.ts
+++ utils/index.ts
@@ -33,7 +33,7 @@ export async function fetchCars(filter: FilterProps) {
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
-  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("customer", `${process.env.IMAGINCustomerKey}`);
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
